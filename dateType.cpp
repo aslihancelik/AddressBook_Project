9354f69405feba6273080f9305c2b0e6d53361f4
aslihancelik <aslihancelik27@gmail.com>
2024-09-23 17:30:49 -0400

    Removeds some commented out lines

--- dateType.cpp
+++ dateType.cpp
@@ -9,61 +9,42 @@ dateType::dateType(int month, int day, int year) {
 }
 
 void dateType::setDate(int month, int day, int year) {
-	//bool isMonthValid, isYearValid;
 
 	//Setting the month
 	if (month >= 1 && month <= 12) {
 		dMonth = month;
-		//isMonthValid = true;
 	}
 	else {
-		//cout << "Invalid month provided.Default 1 assigned." << endl;
 		cout << "Date invalid, setting to 1 - 1 - 1900 "<< endl;
 		dMonth = 1;
 		dDay = 1;
 		dYear = 1900;
-		//isMonthValid = false;     //maybe remove
 		return;
 	}
-
 
 	//Setting the year
 	if (year >= 1900) {
 		dYear = year;
-		//isYearValid = true;
 	}
 	else {
-		//cout << "Invalid year provided.Default 1900 assigned." << endl;
 		cout << "Date invalid, setting to 1 - 1 - 1900 " << endl;
-		//dYear = 1900;
 		dMonth = 1;
 		dDay = 1;
 		dYear = 1900;
-		//isYearValid = false;  //may remove
 		return;
 	}
 
 	//need to add if days in a month is valid
-	//if (isMonthValid == true && isYearValid == true) {
-		if (day >= 1 && day <= getDaysInMonth(month, year)) {
-			dDay = day;
-		}
-		else {
-			//cout << "Invalid day provided. Default 1 assigned." << endl;
-			cout << "Date invalid, setting to 1 - 1 - 1900 " << endl;
-			//dDay = 1;
-			dMonth = 1;
-			dDay = 1;
-			dYear = 1900;
-			//isYearValid = false;  //may remove
-			return;
-		}
-	//}
-	//else {
-	//	cout << "Either invalid month or year provided. Therefore, default of 1 is assigned for day." << endl;
-	//	dDay = 1;
-	//}
-		
+	if (day >= 1 && day <= getDaysInMonth(month, year)) {
+		dDay = day;
+	}
+	else {
+		cout << "Date invalid, setting to 1 - 1 - 1900 " << endl;
+		dMonth = 1;
+		dDay = 1;
+		dYear = 1900;
+		return;
+	}
 }
 
 int dateType::getDaysInMonth(int month, int year) 
