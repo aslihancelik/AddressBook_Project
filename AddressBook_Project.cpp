1aca1fd0696d251cdaef8f8c9918c58733425abe
aslihancelik <aslihancelik27@gmail.com>
2024-09-30 01:57:48 -0400

    removed redundant code

--- AddressBook_Project.cpp
+++ AddressBook_Project.cpp
@@ -1,3 +1,22 @@
+// Client Program for Address Book Management
+//
+// Summary:
+// This client program interacts with the user to manage an online address book through a menu-driven interface. 
+// It allows users to view all entries, search by name, filter by birth month or relationship, and add or display 
+// entries. The program reads data from a file to initialize the address book and sorts it by last name upon each 
+// entry. The menu will continue to display until the user selects the option to quit.
+//
+// Key Features:
+// - Display all entries in the address book.
+// - Search for an entry by last name.
+// - Filter entries by birth month or relationship tag (e.g., Business, Friend, Family).
+// - Menu-driven navigation for user-friendly interaction.
+// - Exits when the user selects the quit option.
+//
+// Programmer: Aslihan Celik
+
+
+
 #include <iostream>
 #include "addressBookType.h"
 
@@ -11,7 +30,7 @@ int showMenu() {
 	cout << "1. Display all entries in the address Book." << endl;
 	cout << "2. Display an entry by a person’s name." << endl;
 	cout << "3. Display all entries with a given birth month." << endl;
-	cout << "4. Display all entries with a given relationship tag(Business, Friend, Family)." << endl;
+	cout << "4. Display all entries with a given relationship tag (Business, Friend, Family)." << endl;
 	cout << "5. Quit" << endl;
 	cin >> choice; ///////////////////////////////////////////////////////////////////////////
 	return choice;
