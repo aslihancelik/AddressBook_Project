29d5de9ad66a1adca8dbb1cadd36c702dfd6ed51
Aslihan Aria Celik <aslihancelik27@gmail.com>
2024-10-26 17:01:22 -0400

    edited comments

--- addressBookType.h
+++ addressBookType.h
@@ -24,17 +24,6 @@ using namespace std;
 
 class addressBookType : public orderedLinkedList<extPersonType>
 {
-	
-//private:
-	/*
-	extPersonType addressList[500];  // Array to hold the entries
-	int length;                      // Current number of entries
-	int maxLength = 500;             // Maximum allowed entries     
-	*/
-
-	//nodeType<extPersonType>* first;  // Pointer to the first node
-	//nodeType<extPersonType>* last;   // Pointer to the last node
-	//int count;                       // Number of nodes in the list
 
 public:
 
